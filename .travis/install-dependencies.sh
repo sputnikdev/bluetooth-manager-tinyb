b8785d8e461294b72516e183b6695e426c82432f
Vlad Kolotov <vkolotov@gmail.com>
2017-09-21 14:54:14 +1200

    Updating versions of tinyb lib Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn install:install-file -Dfile=lib/tinyb.jar -DgroupId=intel-iot-devkit -DartifactId=tinyb -Dversion=0.6.0-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=lib/tinyb.jar -DgroupId=intel-iot-devkit -DartifactId=tinyb -Dversion=0.6.0 -Dpackaging=jar
